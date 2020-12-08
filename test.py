eff284262dab93397d340cfe7ef21f911fe47d9e
LalithSrinivas <srinivaskaladi3096@gmail.com>
2020-12-08 09:58:42 +0530

    using graph-tool

--- test.py
+++ test.py
@@ -1,13 +1,22 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-from random import randrange
+import os
+import graph_tool.all as gt
 
-files = ['1', '2', '3', '4']
+files = [str(i) for i in range(7, 8)]
+val_dir = os.getcwd()+'/val_set/'
 for file in files:
-    img_ = cv2.imread('val_set/'+file+'/'+file+'b.png')
+    files_in_direc = os.listdir(val_dir+file+"/")
+    print(files_in_direc)
+    img_ = cv2.imread(val_dir+file+'/'+files_in_direc[1])
+    frac = 0.4
+    dim = (int(img_.shape[1]*frac), int(img_.shape[0]*frac))
+    img_ = cv2.resize(img_, dim, interpolation = cv2.INTER_AREA)
     img1 = cv2.cvtColor(img_,cv2.COLOR_BGR2GRAY)
-    img = cv2.imread('val_set/'+file+'/'+file+'a.png')
+    img = cv2.imread(val_dir+file+'/'+files_in_direc[0])
+    dim = (int(img.shape[1]*frac), int(img.shape[0]*frac))
+    img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
     img2 = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
     # images = [img_, img]
@@ -39,17 +48,56 @@ for file in files:
         raise AssertionError("Can’t find enough keypoints.")
 
     dst = cv2.warpPerspective(img_,H,(img.shape[1]+img_.shape[1], img.shape[0]+img_.shape[0]))
+    plt.subplot(122),plt.imshow(dst),plt.title('Warped Image')
+    plt.show()
     # plt.figure()
-    dst[0:img.shape[0], 0:img.shape[1]] = img
+    g = gt.Graph()
+    edge_weights = g.new_edge_property('double')
+    g.edge_properties['weight'] = edge_weights
+    first = -1
+    last = -1
+    print(img.shape)
+    src_val = img.shape[0]*img.shape[1]
+    tgt_val = src_val+1
+    for i in range(0, img.shape[0]):
+        last = -1
+        for j in range(0, img.shape[1]):
+            if(dst[i, j].all() == 0):
+                dst[i, j] = img[i, j]
+            elif i < img.shape[0]-1 and j < img.shape[1]-1:
+                if first<i:
+                    first = i
+                    e = g.add_edge(src_val, i*img.shape[1]+j)
+                    edge_weights[e] = np.inf
+                e = g.add_edge(i*img.shape[1]+j, (i+1)*img.shape[1]+j)
+                norm = sum(abs(img[i, j]-dst[i, j])) + \
+                        sum(abs(img[i+1, j]-dst[i+1, j]))
+                edge_weights[e] = norm
+                # e1 = g.add_edge(i*img.shape[1]+j, i*img.shape[1]+j+1)
+                # norm = ((sum((img[i:i+32, j:j+32]-dst[i:i+32, j:j+32])**2))**0.5) + ((sum((img[i:i+32, j+32:j+64]-dst[i:i+32, j+32:j+64])**2))**0.5)
+                # edge_weights[e1] = norm
+                last = j
+            if last!=-1:
+                e = g.add_edge(i*img.shape[1]+last, tgt_val)
+                edge_weights[e] = np.inf
+    print("done")
+    src_index = g.vertex_index[src_val]
+    tgt_index = g.vertex_index[tgt_val]
+    src, tgt = g.vertex(src_index), g.vertex(tgt_index)
+    res = gt.boykov_kolmogorov_max_flow(g, src, tgt, edge_weights)
+    part = gt.min_st_cut(g, src, edge_weights, res)
+    # mc, part = gt.min_cut(g, edge_weights)
+    # pos = g.vertex_properties['pos']
+    for i in range(g.num_vertices()):
+        if int(g.vertex(i)) < img.shape[0]*img.shape[1]:
+            if part[g.vertex(i)]:
+                dst[i//img.shape[1], i%img.shape[1]] = img[i//img.shape[1], i%img.shape[1]]
+                
+    # dst[0:img.shape[0], 0:img.shape[1]] = img
     gray = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
-    # plt.subplot(122),plt.imshow(gray),plt.title('Warped Image')
-    # plt.show()
     _, thresh = cv2.threshold(gray, 1, 255, cv2.THRESH_BINARY)
     _, contour, hei = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnt = max(contour, key=cv2.contourArea)
     x, y, w, h =  cv2.boundingRect(cnt)
     dst = dst[y:y+h, x:x+w]
-    cv2.imwrite('val_set/'+file+'/output.jpg',dst)
-    # plt.imshow(dst)
-    # plt.show()
-
+    cv2.imwrite(val_dir+file+'/output.jpg',dst)
